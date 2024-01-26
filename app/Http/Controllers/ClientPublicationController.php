a10c78fe1ca7e12e5ba1cbe30fe485c0a0c035d0
Zafer A <lssjshisui@gmail.com>
2024-01-26 12:20:16 -0500

    rajout de controllers/models

---
+++ app/Http/Controllers/ClientPublicationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\client_publication;
+use Illuminate\Http\Request;
+
+class ClientPublicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(client_publication $client_publication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(client_publication $client_publication)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, client_publication $client_publication)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(client_publication $client_publication)
+    {
+        //
+    }
+}
