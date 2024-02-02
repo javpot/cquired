f4539081e71f204755122216e2c2ec9823ce34c8
Kenny Rigaud <rigaudkenny@gmail.com>
2024-02-01 21:17:35 -0500

    Accueil partially done

--- resources/js/Components/Accueil.vue
+++ resources/js/Components/Accueil.vue
@@ -33,6 +33,59 @@ export default {
             <h2>Quelques Services</h2>
             <p>Vous voulez voir plus ? <span>Chercher</span></p>
             <div class="carrousel-service">
+                <div >
+                    <div class="carousel" >
+                      <ul class="slides" style="height: 50vh;">
+                        <input type="radio" name="radio-buttons" id="img-1" checked />
+                        <li class="slide-container">
+                          <div class="slide-image">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/9/9e/Timisoara_-_Regional_Business_Centre.jpg">
+                          </div>
+                          <div class="carousel-controls">
+                            <label for="img-3" class="prev-slide">
+                              <span>&lsaquo;</span>
+                            </label>
+                            <label for="img-2" class="next-slide">
+                              <span>&rsaquo;</span>
+                            </label>
+                          </div>
+                        </li>
+                        <input type="radio" name="radio-buttons" id="img-2" />
+                        <li class="slide-container">
+                          <div class="slide-image">
+                            <img src="https://content.r9cdn.net/rimg/dimg/db/02/06b291e8-city-14912-171317ad83a.jpg?width=1750&height=1000&xhint=3040&yhint=2553&crop=true">
+                          </div>
+                          <div class="carousel-controls">
+                            <label for="img-1" class="prev-slide" style="height: 50vh;">
+                              <span>&lsaquo;</span>
+                            </label>
+                            <label for="img-3" class="next-slide" style="height: 50vh;">
+                              <span>&rsaquo;</span>
+                            </label>
+                          </div>
+                        </li>
+                        <input type="radio" name="radio-buttons" id="img-3" />
+                        <li class="slide-container">
+                          <div class="slide-image">
+                            <img src="https://speakzeasy.files.wordpress.com/2015/05/twa_blogpic_timisoara-4415.jpg">
+                          </div>
+                          <div class="carousel-controls">
+                            <label for="img-2" class="prev-slide" style="height: 50vh;"> 
+                              <span>&lsaquo;</span>
+                            </label>
+                            <label for="img-1" class="next-slide" style="height: 50vh;">
+                              <span>&rsaquo;</span>
+                            </label>
+                          </div>
+                        </li>
+                        <div class="carousel-dots">
+                          <label for="img-1" class="carousel-dot" id="img-dot-1"></label>
+                          <label for="img-2" class="carousel-dot" id="img-dot-2"></label>
+                          <label for="img-3" class="carousel-dot" id="img-dot-3"></label>
+                        </div>
+                      </ul>
+                    </div>
+                  </div>
 
                 
 
@@ -232,13 +285,144 @@ font-weight: 600;
     color: #AD12C7;
 }
 /*carrousel a faire*/
-.carrousel-service{
+.carousel {
+    margin-left: 15%;
+    margin-right: 15%;
+}
+
+ul.slides {
+    display: block;
+    position: relative;
+    height: 600px;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    list-style: none;
+}
+
+.slides * {
+    user-select: none;
+    -ms-user-select: none;
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    -webkit-touch-callout: none;
+}
+
+ul.slides input {
+    display: none; 
+}
+
+
+.slide-container { 
+    display: block; 
+}
+
+.slide-image {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    opacity: 0;
+    transition: all .7s ease-in-out;
+}   
+
+.slide-image img {
+    width: auto;
+    min-width: 100%;
+    height: 100%;
+}
+
+.carousel-controls {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 999;
+    font-size: 100px;
+    line-height: 600px;
+    color: #fff;
+}
+
+.carousel-controls label {
+    display: none;
+    position: absolute;
+    padding: 0 20px;
+    opacity: 0;
+    transition: opacity .2s;
+    cursor: pointer;
+}
+
+.slide-image:hover + .carousel-controls label{
+    opacity: 0.5;
+}
+
+.carousel-controls label:hover {
+    opacity: 1;
+}
+
+.carousel-controls .prev-slide {
+    width: 49%;
+    text-align: left;
+    left: 0;
+}
+
+.carousel-controls .next-slide {
+    width: 49%;
+    text-align: right;
+    right: 0;
+}
+
+.carousel-dots {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 20px;
+    z-index: 999;
+    text-align: center;
+}
+
+.carousel-dots .carousel-dot {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #fff;
+    opacity: 0.5;
+    margin: 10px;
+}
+
+input:checked + .slide-container .slide-image {
+    opacity: 1;
+    transform: scale(1);
+    transition: opacity 1s ease-in-out;
+}
+
+input:checked + .slide-container .carousel-controls label {
+     display: block; 
+}
+
+input#img-1:checked ~ .carousel-dots label#img-dot-1,
+input#img-2:checked ~ .carousel-dots label#img-dot-2,
+input#img-3:checked ~ .carousel-dots label#img-dot-3,
+input#img-4:checked ~ .carousel-dots label#img-dot-4,
+input#img-5:checked ~ .carousel-dots label#img-dot-5,
+input#img-6:checked ~ .carousel-dots label#img-dot-6 {
+	opacity: 1;
+}
+
+
+input:checked + .slide-container .nav label { display: block; }
+
+
+/*.carrousel-service{
     height: 200px;
     width: 200px;
     position: relative;
     left: 25%;
     background-color: #AD12C7;
-}
+}*/
 .section-3{
   min-height: 150vh;
     width: 100vw;
@@ -281,7 +465,6 @@ font-weight: 600;
     
 }
 .section-3-img-container{
-width: 100;
 
 }
 .section-3-img-container>img{
@@ -295,9 +478,17 @@ width: 100;
     display: flex;
     justify-content: center;
     align-items: center;
+   
 }
 .section-4-info{
     width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 5px solid black;
+   /* border-top-left-radius: 55px;
+    border-bottom-left-radius: 55px;*/
 }
 .section-4-text{
     background-color:#0d094c;
@@ -325,6 +516,7 @@ width: 100;
     /*for animation*/
    transition: 0.3s;
 }
+
 .section-4-btn-container>button:hover{
     
     color: white;
@@ -343,6 +535,9 @@ width: 100;
 }
 .section-5-info{
     width: 90%;
+    border: 5px solid black;
+   /* border-top-right-radius: 55px;
+    border-bottom-right-radius: 55px;*/
 }
 .section-5-text{
     background-color:#AD12C7;
@@ -454,11 +649,20 @@ hr{
 
 /*section 1 responsive*/
 @media screen and (min-width: 400px) {
+    
     .section-1-info>h2{
         font-size: 1.3em;
     }
     .section-1-info>h1{
         font-size: 2.1em;
+    }
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 2em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 2em;
     }
     
 
@@ -472,6 +676,14 @@ hr{
         width: 85%;
     }
 
+     /*section 2*/
+     .section-2-info>h2{
+        font-size: 2.2em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 2.2em;
+    }
 }
 @media screen and (min-width: 600px) {
 
@@ -482,6 +694,14 @@ hr{
         font-size: 2.3em;
         width: 72%;
     }
+     /*section 2*/
+     .section-2-info>h2{
+        font-size: 2.4em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 2.4em;
+    }
 }
 @media screen and (min-width: 700px) {
     .section-1-info>h2{
@@ -491,7 +711,24 @@ hr{
         font-size: 3em;
         width: 64%;
     }
-
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 2.6em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 2.6em;
+    }
+     /*section4*/
+     .section-4-text>h2{
+        font-size: 2.6em;
+    }
+     /*section5*/
+     .section-5-text>h2{
+        font-size: 2.6em;
+    }
+    
+    
 }
 @media screen and (min-width: 800px) {
     .section-1-info>h2{
@@ -500,6 +737,108 @@ hr{
     .section-1-info>h1{
         font-size: 3.2em;
         width: 57%;
+    }
+     /*section 2*/
+     .section-2-info>h2{
+        font-size: 2.8em;
+    }
+    /*section 3*/
+    /*a partir de la que le flex change*/
+    .section-3-info>h2{
+        font-size: 2.8em;
+    }
+    .section-3{
+        display: flex;
+        flex-direction: row-reverse;
+        
+        align-items: center;
+        justify-content: space-around;
+    }
+    .section-3-btn-container{
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+    .section-3-info{
+        width: 60%;
+        height: 70vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    
+    }
+    .section-3-img-container>img{
+        height: 400px;
+        width: 400px;
+    }
+    /*section 4*/
+    /*a partir de la quon change le flex*/
+    /*section4*/
+    .section-4-text>h2{
+        font-size: 2.8em;
+    }
+    .section-4{
+        height: 100vh;
+        width: 100vw;
+        
+    }
+    .section-4-info{
+        height:80%;
+        width: 90%;
+        display: flex;
+        flex-direction: row-reverse;
+      
+    }
+    .section-4-img-container{
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .section-4-img-container>img{
+        height: 100%;
+    }
+    .section-4-text{
+    
+        height: 100%;
+      /*  border-top-left-radius: 50px;
+        border-bottom-left-radius: 50px;*/
+    }
+    
+
+    /*section 5 a partir de la quon change le flex*/
+     /*section5*/
+     .section-5-text>h2{
+        font-size: 2.8em;
+    }
+    .section-5{
+        height: 100vh;
+        width: 100vw;
+      
+        
+    }
+    .section-5-info{
+        height:80%;
+        width: 90%;
+        display: flex;
+        flex-direction: row;
+        
+      
+    }
+    .section-5-img-container{
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .section-5-img-container>img{
+        height: 100%;
+    }
+    .section-5-text{
+    
+        height: 100%;
+       /* border-top-right-radius: 50px;
+        border-bottom-right-radius: 50px;*/
     }
 
 }
@@ -511,6 +850,14 @@ hr{
         font-size: 3.4em;
         width: 71%;
     }
+     /*section 2*/
+     .section-2-info>h2{
+        font-size: 3em;
+    }
+     /*section 3*/
+     .section-3-info>h2{
+        font-size: 3em;
+    }
 
 }
 @media screen and (min-width: 1000px) {
@@ -521,7 +868,14 @@ hr{
         font-size: 3.5em;
         width: 71%;
     }
-
+      /*section 2*/
+      .section-2-info>h2{
+        font-size: 3.2em;
+    }
+     /*section 3*/
+     .section-3-info>h2{
+        font-size: 3.2em;
+    }
 }
 @media screen and (min-width: 1100px) {
     .section-1-info>h2{
@@ -530,6 +884,14 @@ hr{
     .section-1-info>h1{
         font-size: 3.6em;
         width: 71%;
+    }
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 3.4em;
+    }
+     /*section 3*/
+     .section-3-info>h2{
+        font-size: 3.4em;
     }
 
 }
@@ -541,6 +903,14 @@ hr{
         font-size: 3.7em;
         width: 60%;
     }
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 3.6em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 3.6em;
+    }
 
 }
 @media screen and (min-width: 1300px) {
@@ -550,6 +920,14 @@ hr{
     .section-1-info>h1{
         font-size: 4em;
         width: 70%;
+    }
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 3.8em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 3.8em;
     }
 
 
@@ -562,7 +940,14 @@ hr{
         font-size: 4.2em;
         width: 75%;
     }
-
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 4em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 4em;
+    }
 }
 @media screen and (min-width: 1500px) {
     .section-1-info>h2{
@@ -571,6 +956,14 @@ hr{
     .section-1-info>h1{
         font-size: 4.4em;
         width: 75%;
+    }
+    /*section 2*/
+    .section-2-info>h2{
+        font-size: 4.2em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 4.2em;
     }
 
 }
@@ -582,6 +975,14 @@ hr{
         font-size: 4.6em;
         width: 80%;
     }
+     /*section 2*/
+    .section-2-info>h2{
+        font-size: 4.4em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 4.4em;
+    }
 
 }
 @media screen and (min-width: 1700px) {
@@ -592,7 +993,16 @@ hr{
         font-size: 4.8em;
         width: 85%;
     }
+     /*section 2*/
+     .section-2-info>h2{
+        font-size: 4.6em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 4.6em;
+    }
 
+    
 }
 @media screen and (min-width: 1800px) {
     .section-1-info>h2{
@@ -600,6 +1010,14 @@ hr{
     }
     .section-1-info>h1{
         font-size: 5em;
+    }
+     /*section 2*/
+     .section-2-info>h2{
+        font-size: 4.8em;
+    }
+    /*section 3*/
+    .section-3-info>h2{
+        font-size: 4.8em;
     }
 
 }
