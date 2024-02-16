b16f702f8df67a4ccf7b5e817a68fdc283a56d3e
Zafer A <lssjshisui@gmail.com>
2024-02-16 16:35:54 -0500

    Forfait Vue

---
+++ resources/js/Components/ForfaitCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+defineProps({
+    title: String,
+    price: Number,
+    description: String,
+    features: [String],
+});
+</script>
+
+<template>
+    <div
+        class="h-full w-1/5 bg-white rounded-lg shadow-md flex justify-center items-center p-2"
+    >
+        <div
+            class="h-full w-full flex flex-col rounded-lg justify-between p-10 bg-myBlue"
+        >
+            <h2 class="text-2xl">{{ title }}</h2>
+            <p class="text-white">
+                <span class="text-3xl text-black">$</span
+                ><span class="text-3xl text-black">{{ price }}</span> / Month
+            </p>
+            <p class="text-white text-lg">{{ description }}</p>
+            <p class="text-white text-lg">{{ title }} features:</p>
+            <ul>
+                <li v-for="item in features">&#10003; {{ item }}</li>
+            </ul>
+            <PrimaryButton>Select</PrimaryButton>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Inscription/InsForfait.vue
+++ resources/js/Pages/Inscription/InsForfait.vue
@@ -1,136 +1,132 @@
 <script>
+import ForfaitCardVue from "@/Components/ForfaitCard.vue";
+
 export default {
     data() {
         return {};
     },
+    components: { ForfaitCardVue },
 };
 </script>
 
 <template>
-
-    <div class="container-inscription-section-1  flex flex-column justify-center items-center">
+    <div
+        class="container-inscription-section-1 flex flex-column justify-center items-center"
+    >
         <div class="model-client-freelancer">
-            <div class="progress-bar">
-            </div>
-            <div class="progress-bar-1">
-            </div>
-            <h2 class="text-xl text-center w-1/5">Par mois</h2>
+            <div class="progress-bar"></div>
+            <div class="progress-bar-1"></div>
+            <h2 class="text-xl text-center w-1/5">Subscriptions</h2>
 
             <div class="model-client-freelancer-info">
-            <div>
-                
-              <div class="card-subscription">
-                <h3>Starter plan</h3>
-                <div class="ligne"></div>
-                <li>&#10003; Free</li>
-                <li>&#10003; Access up to 25 people</li>
-                <li>&#10003; Access up to 25 people</li>
-              </div>
-              <div class="card-subscription">
-                <h3>Basic plan</h3>
-                <div class="ligne"></div>
-                <li>&#10003; Free</li>
-                <li>&#10003; Access up to 25 people</li>
-                <li>&#10003; Access up to 25 people</li>
-
-
-              </div>
-              <div class="card-subscription">
-                <h3>Business plan</h3>
-                <div class="ligne"></div>
-                <li>&#10003; Free</li>
-                <li>&#10003; Access up to 25 people</li>
-                <li>&#10003; Access up to 25 people</li>
-
-
-              </div>
-              <div class="card-subscription">
-                <h3>Entreprise plan</h3>
-                <div class="ligne"></div>
-                <li>&#10003; Free</li>
-                <li>&#10003; Access up to 25 people</li>
-                <li>&#10003; Access up to 25 people</li>
-                
-
-
-              </div>
-                
-                
-                
-
-                
-
-                
+                <div class="flex flex-row">
+                    <ForfaitCardVue
+                        title="Starter"
+                        price="0.00"
+                        description="Join
+                    our Starter plan"
+                        :features="[
+                            'limite of 25 people',
+                            'limite of 25 messages',
+                            'search feature',
+                            'messaging feature',
+                        ]"
+                    />
+                    <ForfaitCardVue
+                        title="Basic"
+                        price="19.99"
+                        description="Join
+                    our Basic plan"
+                        :features="[
+                            'limite of 25 people',
+                            'limite of 25 messages',
+                            'limite of 25 messages',
+                            'limite of 25 messages',
+                        ]"
+                    />
+                    <ForfaitCardVue
+                        title="Business"
+                        price="39.99"
+                        description="Join
+                    our Business plan"
+                        :features="[
+                            'limite of 25 people',
+                            'limite of 25 messages',
+                            'limite of 25 messages',
+                            'limite of 25 messages',
+                        ]"
+                    />
+                    <ForfaitCardVue
+                        title="Enterprise"
+                        price="69.99"
+                        description="Join
+                    our Enterprise plan"
+                        :features="[
+                            'special icon',
+                            'limite of 25 messages',
+                            'limite of 25 messages',
+                            'limite of 25 messages',
+                        ]"
+                    />
+                </div>
             </div>
 
-       <!--<button class="continue-btn">Continue</button>-->  
-           
+            <!--<button class="continue-btn">Continue</button>-->
         </div>
     </div>
-
-
-    </div>
-
 </template>
 
 <style scoped>
-h3{
-    margin-left: 10%;
-}
-.ligne{
+.ligne {
     height: 2px;
     width: 80%;
     position: relative;
     left: 10%;
     background-color: black;
 }
-li{
+li {
     list-style-type: none;
     margin-left: 10%;
 }
 
-.container-inscription-section-1{
-    height:90vh;
+.container-inscription-section-1 {
+    height: 90vh;
     width: 100vw;
 }
-.model-client-freelancer{
+.model-client-freelancer {
     height: 90%;
     width: 90%;
     background-color: white;
-display: flex;
-flex-direction: column;
+    display: flex;
+    flex-direction: column;
 
-box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
-.progress-bar{
+.progress-bar {
     height: 5%;
     width: 75%;
-    background-color: #6EC3E7;;
+    background-color: #6ec3e7;
     position: relative;
-    bottom : 0.6%;
+    bottom: 0.6%;
     z-index: 2;
 }
-.progress-bar-1{ 
+.progress-bar-1 {
     height: 5%;
     width: 100%;
-    background-color: #D9D9D9;
+    background-color: #d9d9d9;
     position: relative;
     bottom: 4.5%;
     z-index: 1;
-   
-
 }
-.model-client-freelancer-info{
+.model-client-freelancer-info {
     height: 90%;
     width: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: space-evenly;
     align-items: center;
-
 }
-.model-client-freelancer-info>div{
+.model-client-freelancer-info > div {
     height: 80%;
     width: 100%;
     display: flex;
@@ -139,46 +135,68 @@ box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     gap: 15px;
     justify-content: space-around;
     align-items: center;
-   
-    
 }
-.card-subscription{
+.card-subscription {
     height: 100%;
     width: 20%;
-    
-    background-color: #a6d0e3;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-  
+
+    background-color: #fff;
+    border-radius: 10px;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-}
-.card-subscription:nth-child(2){
-    
-    background-color: #6EC3E7;
-}
-.card-subscription:nth-child(3){
-    
-    background-color: #3ab3e7;
-}
-.card-subscription:nth-child(4){
-    
-    background-color: #0f9edc;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-img{
+.card-content {
+    height: 95%;
+    width: 92%;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    justify-content: space-around;
+    padding: 10px;
+}
+
+h2 {
+    font-size: 25px;
+}
+
+h3 {
+    font-size: 18px;
+}
+
+.card-content p {
+    font-size: 20px;
+}
+
+.amount {
+    font-size: 40px;
+    color: black;
+}
+.money {
+    font-size: 30px;
+    color: black;
+}
+
+.button {
+    width: 90%;
+}
+
+img {
     height: 100px;
     width: 150px;
 }
-.check-blue{
+.check-blue {
     height: 50px;
     width: 50px;
     position: relative;
-    left:80px;
+    left: 80px;
     top: 100px;
     display: none;
 }
-.selection-btn{
+.selection-btn {
     height: 50px;
     width: auto;
     padding-left: 10px;
@@ -190,70 +208,64 @@ img{
     border: 1px solid #adadad;
     transition: 0.3s;
 }
-.selection-btn:hover{
-    border: 1px solid #6EC3E7;
-    color: #6EC3E7;
+.selection-btn:hover {
+    border: 1px solid #6ec3e7;
+    color: #6ec3e7;
 }
 
-.continue-btn{
+.continue-btn {
     height: 50px;
     width: 50%;
-    background-color: #6EC3E7;
+    background-color: #6ec3e7;
     text-align: center;
     border-radius: 30px;
     color: black;
 }
 @media screen and (min-width: 900px) {
-    .container-inscription-section-1{
-    height: 90vh;
-    width: 100vw;
-}
+    .container-inscription-section-1 {
+        height: 90vh;
+        width: 100vw;
+    }
 
-
-    .model-client-freelancer{
+    .model-client-freelancer {
         height: 90%;
         width: 95%;
         background-color: white;
-    display: flex;
-    flex-direction: column;
-    
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-    
+        display: flex;
+        flex-direction: column;
+
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     }
-    input{
+    input {
         width: 100%;
     }
-    .model-client-freelancer-info{
+    .model-client-freelancer-info {
         height: 90%;
         width: 100%;
         display: flex;
-        flex-direction:column;
+        flex-direction: column;
         justify-content: space-evenly;
         align-items: center;
-    
     }
-    img{
+    img {
         height: 150px;
         width: 200px;
     }
-    .progress-bar{
+    .progress-bar {
         height: 5%;
         width: 75%;
-        background-color: #6EC3E7;;
+        background-color: #6ec3e7;
         position: relative;
-        bottom : 0.3%;
+        bottom: 0.3%;
         z-index: 2;
     }
-    .progress-bar-1{ 
+    .progress-bar-1 {
         height: 5%;
         width: 100%;
-        background-color: #D9D9D9;
+        background-color: #d9d9d9;
         position: relative;
         bottom: 5%;
         z-index: 1;
-       
-    
     }
-
 }
 </style>
